7a17a57075f7c751b3da3907a92ff4008dbb1a60
Parthipan <parthipan@group-ea.ae>
2022-11-14 13:32:26 +0530

    first commit

---
+++ basic/Controllers/TestController.cs
@@ -0,0 +1,25 @@
+﻿using basic.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace basic.Controllers
+{
+    public class TestController : Controller
+    {
+        // GET: Test
+        public ActionResult Index()
+        {
+            List<Employee> employeesList = new List<Employee>(); 
+            Employee employee = new Employee();
+            employeesList.Add(new Employee { Id = 1, Name = "parthipan", Deportment = "mech" });
+            employeesList.Add(new Employee { Id = 2, Name = "sasi", Deportment = "cse" });
+            employeesList.Add(new Employee { Id = 3, Name = "narmathaparthipan**", Deportment = "cse" });
+            employeesList.Add(new Employee { Id = 4, Name = "nish", Deportment = "EEE" });
+            employeesList.Add(new Employee { Id = 5, Name = "mani", Deportment = "IT" });
+            return View(employeesList);
+        }
+    }
+}
